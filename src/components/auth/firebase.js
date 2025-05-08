5c6aca3d2c908c701cf982eec9f1cc8817386893
Aidriel2002 <147413687+Aidriel2002@users.noreply.github.com>
2025-05-08 08:55:34 +0000

    f

--- src/components/auth/firebase.js
+++ src/components/auth/firebase.js
@@ -1,26 +1,20 @@
-
-
-// firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3pWYWH0RA_Fn9Pu1ZlqqGlJiUWigYybs",
-  authDomain: "nieldo-1ef3a.firebaseapp.com",
-  projectId: "nieldo-1ef3a",
-  storageBucket: "nieldo-1ef3a.appspot.com",
-  messagingSenderId: "410082067433",
-  appId: "1:410082067433:web:d9cb3920d9f0cb3b2a0178"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Use getFirestore to get Firestore instance
 const db = getFirestore(app);
 
-// Use getAuth to get Auth instance
 const auth = getAuth(app);
 
 export { app, db, auth };
